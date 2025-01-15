3b9aae298813da6cef732bd5bc12a46e9ca5be08
lourenasty <Anastasieshcherbyna@gmail.com>
2025-01-15 23:58:10 +0200

    Завдання №1

---
+++ Car/Car.java
@@ -0,0 +1,26 @@
+package Car;
+
+public class Car {
+    private final String make;
+    private final String model;
+    private final int year;
+    private double mileage = 0;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void increaseMileage(double additionalMileage) {
+        mileage += additionalMileage;
+    }
+
+    public void displayInfo() {
+        System.out.println("Марка автомобіля: " + make);
+        System.out.println("Модель автомобіля: " + model);
+        System.out.println("Рік випуску: " + year);
+        System.out.println("Пробіг: " + mileage);
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,10 @@
+import Car.Car;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car("BMW", "X5", 2010);
+        car.increaseMileage(10678.056);
+        car.displayInfo();
+    }
+}
