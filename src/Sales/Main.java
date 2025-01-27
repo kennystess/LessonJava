1f509bf42580bf326e86e7ca74db61c3731fb2d3
lourenasty <Anastasieshcherbyna@gmail.com>
2025-01-27 20:59:02 +0200

    Домашнє завдання

---
+++ src/Sales/Main.java
@@ -0,0 +1,18 @@
+import src.Sales.Sales;
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter total price: ");
+    double totalPrice = scanner.nextDouble();
+
+    System.out.print("Are you VIP client? ");
+    boolean isVip = scanner.nextBoolean();
+
+    var discount = Sales.getDiscount(totalPrice, isVip);
+
+    System.out.println(" ");
+    System.out.println("Total price before discount: " + totalPrice);
+    System.out.println("Discount: " + discount * 100 + "%");
+    System.out.println("Total price after discount: " + (totalPrice - totalPrice * discount));
+}
---
+++ src/Sales/Sales.java
@@ -0,0 +1,22 @@
+package src.Sales;
+
+import java.util.Scanner;
+
+public class Sales {
+    public static double getDiscount(double totalPrice, boolean isVip) {
+        double discount = 0;
+
+        if (totalPrice > 10_000) {
+            discount = 0.15;
+        } else if (totalPrice > 5000 ) {
+            discount = 0.10;
+        } else if (totalPrice > 1000) {
+            discount = 0.05;
+        }
+        if (isVip){
+            discount += 0.05;
+        }
+
+        return discount;
+    }
+}
