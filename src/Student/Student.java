33c609e860ade038fe66823563c96bcf9779ec84
lourenasty <Anastasieshcherbyna@gmail.com>
2025-01-28 17:35:21 +0200

    homework

--- src/Student/Student.java
+++ src/Student/Student.java
@@ -8,22 +8,26 @@ public class Student {
     String lastName;
     String studentID;
     List<Integer> grades;
-    public Student(String firstName, String lastName,String studentID) {
-     this.firstName = firstName;
-     this.lastName = lastName;
-     this.studentID = studentID;
-     this.grades = new ArrayList<>();
+
+    public Student(String firstName, String lastName, String studentID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentID = studentID;
+        this.grades = new ArrayList<>();
     }
-    public void addGrade(int grade){
+
+    public void addGrade(int grade) {
         grades.add(grade);
     }
+
     public double calculateAverageGrade() {
         int sum = 0;
-        for (int grade : grades){
+        for (int grade : grades) {
             sum += grade;
         }
         return (double) sum / grades.size();
     }
+
     public void displayInfo() {
         System.out.println("Вся інформація про студента: ");
         System.out.println("Ім'я: " + firstName);
--- src/Student/StudentMain.java
+++ src/Student/StudentMain.java
@@ -4,7 +4,7 @@ import src.Student.Student;
 
 public class StudentMain {
     public static void main(String[] args) {
-        Student student = new Student("Bella", "Leyla", "KA23768" );
+        Student student = new Student("Bella", "Leyla", "KA23768");
         student.addGrade(50);
         student.addGrade(97);
         student.addGrade(52);
